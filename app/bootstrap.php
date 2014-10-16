a64155bd392e7aca8eebed345a0615baec645ee3
André Figueira <andre.figueira@prism-dm.co.uk>
2014-10-16 11:55:26 +0100

    - Fix for allowing the autoload to be loaded even if installed as a vendor instead of standalone.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,44 @@
 <?php
+/**
+ * This bootstrap file handles the including of the autoloader and sets up some constants for the application
+ */
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+try
+{
+
+    //Setup the autoloader dir
+    $autoloadFile = dirname(__DIR__) . '/vendor/autoload.php';
+
+    //Check if exists like this, it will be in this path if standalone
+    if(!file_exists($autoloadFile))
+    {
+
+        //Not installed standalone, this is a vendor, check above
+        $autoloadFile = dirname(dirname(dirname(__DIR__))) . '/autoload.php';
+
+    }
+
+    //Finally check again and include or throw error
+    if(file_exists($autoloadFile))
+    {
+
+        require_once $autoloadFile;
+
+    }
+    else
+    {
+
+        throw new Exception('Cannot find autoloader... Please run composer install');
+
+    }
+
+}
+catch(\Exception $e)
+{
+
+    echo $e->getMessage();
+
+}
 
 date_default_timezone_set('UTC');
 
