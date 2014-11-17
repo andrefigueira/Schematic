654718f09a698c859f3298fe337d94d0052f8f79
André Figueira <andre.figueira@me.com>
2014-11-17 18:27:44 +0000

    - Fix for the Schematic title so it only appears where the app name is used, nowhere else.

--- cli.php
+++ cli.php
@@ -13,15 +13,6 @@ use Library\Cli\SchematicGeneratorConsoleApp;
 use Library\Cli\SchematicMappingImportConsoleApp;
 use Symfony\Component\Console\Application;
 
-echo "
-   _____      __                         __  _
-  / ___/_____/ /_  ___  ____ ___  ____ _/ /_(______
-  \__ \/ ___/ __ \/ _ \/ __ `__ \/ __ `/ __/ / ___/
- ___/ / /__/ / / /  __/ / / / / / /_/ / /_/ / /__
-/____/\___/_/ /_/\___/_/ /_/ /_/\__,_/\__/_/\___/
-
-";
-
 $application = new Application(APP_NAME, APP_VERSION);
 
 $application->add(new SchematicConsoleApp);
