75da19d67ce150b47022766b4e620e6919de2bf8
André Figueira <andre.figueira@me.com>
2014-11-18 23:43:54 +0000

    - Commenting is your friend

--- app/Library/Migrations/Configurations.php
+++ app/Library/Migrations/Configurations.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * This class handles the configurations for the Schematic setup, checks the file types and loads config data
+ */
 
 namespace Library\Migrations;
 
@@ -7,16 +10,29 @@ use Library\Cli\OutputInterface;
 class Configurations
 {
 
+    /** @var bool The file type being used */
     public $fileType;
 
+    /** @var bool The variable which determines if a config file exists */
     public $configFileExists = false;
 
+    /** @var string The file path for the config file */
     public $configFilePath;
 
+    /** @var object A stdObj which is filled with data from a reflection call that loads config data from the config file */
     public $config;
 
+    /** @var string The prefix of the default schematic configuration file */
     const CONFIG_FILE_NAME = '.schematic';
 
+    /**
+     * The construct expects an output interface so it can print to screen it also runs the getConfigFileType to determine
+     * what the file type is on it's own, and after checks if it can fetch configuration information from the config file
+     * if it exists
+     *
+     * @param OutputInterface $output
+     * @throws \Exception
+     */
     public function __construct(OutputInterface $output)
     {
 
@@ -28,6 +44,12 @@ class Configurations
 
     }
 
+    /**
+     * Loops through the allowed config file types and checks if any schematic config file exists for the project and it
+     * returns the file type it's found if there is one
+     *
+     * @return bool
+     */
     private function getConfigFileType()
     {
 
@@ -54,6 +76,12 @@ class Configurations
 
     }
 
+    /**
+     * Uses the RelectionMethod class to try and call a fileApiAdapter class to convert the formatted contents of a configuration
+     * file into a stdObject which we can use
+     *
+     * @throws \Exception
+     */
     private function fetchConfigurations()
     {
 
@@ -83,6 +111,11 @@ class Configurations
 
     }
 
+    /**
+     * Returns an array of the allowed configuration file types
+     *
+     * @return array
+     */
     private function allowedConfigurationFileTypes()
     {
 
