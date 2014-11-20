8c883cad7ecaa50b8d88d7769fc054c039c06872
André Figueira <andre.figueira@me.com>
2014-11-20 00:46:27 +0000

    - Don't you hate it when you forget to commit one fucking file...

--- tests/SchematicHelperTest.php
+++ tests/SchematicHelperTest.php
@@ -13,46 +13,6 @@ class SchematicHelperTest extends PHPUnit_Framework_TestCase
 
     }
 
-    /**
-     * Checks that we are returning an array still for the checking of database drivers
-     */
-    public function testValidDatabaseDriversReturnsArray()
-    {
-
-        $this->assertTrue(is_array(\Library\Helpers\SchematicHelper::validDatabaseDrivers()));
-
-    }
-
-    /**
-     * Checks that the validFileTypes return a valid array
-     */
-    public function testValidFileTypesReturnsArray()
-    {
-
-        $this->assertTrue(is_array(\Library\Helpers\SchematicHelper::validFileTypes()));
-
-    }
-
-    /**
-     * Checks to see if we get a valid file api adapter from the adapter fetcher
-     */
-    public function testGetFileTypeGeneratorAdapterReturnsInstanceOfYamlAdapterClass()
-    {
-
-        $this->assertInstanceOf('Library\Migrations\FileApi\Adapters\YamlAdapter', \Library\Helpers\SchematicHelper::getFileTypeGeneratorAdapter('yaml', $this->consoleOutput));
-
-    }
-
-    /**
-     * Checks to see if we get a valid file api adapter from the adapter fetcher
-     */
-    public function testGetFileTypeGeneratorAdapterReturnsInstanceOfJsonAdapterClass()
-    {
-
-        $this->assertInstanceOf('Library\Migrations\FileApi\Adapters\JsonAdapter', \Library\Helpers\SchematicHelper::getFileTypeGeneratorAdapter('json', $this->consoleOutput));
-
-    }
-
     public function testInitReturnsArray()
     {
 
